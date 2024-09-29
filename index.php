1e808b9130843cd83c5991fb144fc674564703ed
David <champion.pokemon327@gmail.com>
2024-09-29 14:58:05 -0500

    Agregar estructura inicial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page test</title>
+    <link rel="stylesheet" href="css/styles.css">
+<!-- Boostrap Space -->
+</head>
+<body>
+    <header>
+        <h1>
+            Bienvenido a mi landing page
+        </h1>
+    </header>
+    <main>
+        <p>
+            Contenido de la pagina aquí
+        </p>
+    </main>
+    
+    <footer>
+        <p>
+            &copy; 2024 David Martinez
+        </p>
+    </footer>
+</body>
+</html>
