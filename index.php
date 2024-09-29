12a0a94df785bb18e04087fd6a5694702039ed3c
David <champion.pokemon327@gmail.com>
2024-09-29 15:44:40 -0500

    Se agragaron las secciones y las dependencias de Boostrap

--- index.php
+++ index.php
@@ -4,25 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page test</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 <!-- Boostrap Space -->
 </head>
 <body>
-    <header>
+    <header class="bg-primary text-white text-center p-4">
         <h1>
             Bienvenido a mi landing page
         </h1>
-    </header>
-    <main>
         <p>
-            Contenido de la pagina aquí
+            Esta es una breve descripción de lo que ofrecemos
         </p>
-    </main>
+    </header>
+
+    <div class="container my-5">
+        <section>
+            <h2>
+                ¿Quienes somos?
+            </h2>
+            <p>
+                Aquí puedes hablar sobre tu proyecto o empresa
+            </p>
+        </section>
+
+        <section>
+            <h2>Servicios</h2>
+            <ul>
+                <li>Servicio 1</li>
+                <li>Servicio 2</li>
+                <li>Servicio 3</li>
+            </ul>
+        </section>
+
+        <Section>
+            <h2>Contáctanos</h2>
+            <p>Proporciona información de contacto o un formulario aquí</p>
+        </Section>
+
+    </div>
+
     
-    <footer>
+    <footer class="bg-dark text-white text-center p-4">
         <p>
-            &copy; 2024 David Martinez
+            &copy; 2024 Mi empresa. Todos los derechos reservados.
         </p>
     </footer>
+
+    <!-- Vincular Bootstrap JS y dependencias -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
